9b6aa089b8ca16c3cb02628088d94e496c2b264a
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-16 14:40:34 +0100

    add functions fix code

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+/*---------------
+    FUNCTION
+ ----------------*/
+
+ function createPost (element){
+    const card = `
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${element.author.name}</div>
+                <div class="post-meta__time">${element.created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">"${element.content}"</div>
+    <div class="post__image">
+        <img src="${element.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+</div>
+    `
+    return card;
+
+ }
+
+
 const posts = [
     {
         "id": 1,
@@ -61,38 +103,8 @@ const postContainer = document.querySelector(".posts-list");
 for(i=0 ; i < posts.length ; i++){
     const element = posts[i];
     console.log(element);
-    const card = `
-    <div class="post">
-    <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
-            </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">${element.author.name}</div>
-                <div class="post-meta__time">${element.created}</div>
-            </div>                    
-        </div>
-    </div>
-    <div class="post__text">"${element.content}"</div>
-    <div class="post__image">
-        <img src="${element.media}" alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
-            </div>
-        </div> 
-    </div>            
-</div>
-    `
+    const card = createPost(element);
+    
     postContainer.innerHTML += card;
 }
 
